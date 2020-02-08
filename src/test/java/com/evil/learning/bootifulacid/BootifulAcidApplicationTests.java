62afb96cac4982507a8c6bfedd602a7eff3633b8
Ion Pascari <ipascari@isd-soft.com>
2020-02-08 20:40:13 +0200

    Initialize project

---
+++ src/test/java/com/evil/learning/bootifulacid/BootifulAcidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evil.learning.bootifulacid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootifulAcidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
